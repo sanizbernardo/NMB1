faeeee82dfbb17cdf313a2dee606e61e16d3798a
Elias Storme <elias.storme@gmail.com>
2018-04-07 14:46:32 +0200

    added output suppression to where it was forgotten

--- section2/deboor.m
+++ section2/deboor.m
@@ -1,7 +1,7 @@
 function y = deboor(t,x,c_given,k)
     y = zeros(length(x),1);
     for i = 1:length(x)
-        j = chooseStartKnot(x(i),t)
+        j = chooseStartKnot(x(i),t);
         % shortens both vectors to the range that will actually be used for
         % calculation. this is done to make the indices of the vector more 
         % compatible with the numbers used by the loop counter
@@ -9,7 +9,6 @@ function y = deboor(t,x,c_given,k)
         for m = 1:k
             cn = zeros(length(c),1);
             for n = j-k+m:j
-                t(n)
                 alpha = (x(i) - t(n))/(t(n+k+1-m)-t(n));
                 cn(n) = alpha*c(n) + (1-alpha)*c(n-1);
             end
