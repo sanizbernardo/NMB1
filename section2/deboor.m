d42c96cf194ab68e1642c9da8f0b6c26e6ae4924
Elias Storme <elias.storme@gmail.com>
2018-04-07 17:36:47 +0200

    removed comments that were no longer applicable, needs some documenting though

--- section2/deboor.m
+++ section2/deboor.m
@@ -1,13 +1,10 @@
 function y = deboor(t,x,c_given,k)
     y = zeros(length(x),1);
     for i = 1:length(x)
-        j = chooseStartKnot(x(i),t);
-        % shortens both vectors to the range that will actually be used for
-        % calculation. this is done to make the indices of the vector more 
-        % compatible with the numbers used by the loop counter
+        j = chooseStartKnot(x(i),t,k);
         c = c_given;
+        cn = zeros(length(c),1);
         for m = 1:k
-            cn = zeros(length(c),1);
             for n = j-k+m:j
                 alpha = (x(i) - t(n))/(t(n+k+1-m)-t(n));
                 cn(n) = alpha*c(n) + (1-alpha)*c(n-1);
