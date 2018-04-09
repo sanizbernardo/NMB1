f6ab5725085d3a1501c12e8c36a592da57743552
Elias Storme <elias.storme@gmail.com>
2018-04-09 14:45:42 +0200

    made some optimisations

--- section2/evaluate_bspline.m
+++ section2/evaluate_bspline.m
@@ -6,24 +6,19 @@
 % note: for efficiency 
 function y = evaluate_bspline(t,x,k)
     n = length(t) - 2*k - 1;
-    if length(x)~=1
-        y = zeros(length(x),n+k);
-        for i = 1:length(x)
-            y(i,:) = evaluate_bspline(t,x(i),k);
-        end
-    else
-        j = chooseStartKnot(x,t,k);
-        y = zeros(k+n,1);
-        yn = y;
-        y(j) = 1;
-        for m = 1:k
-            yn(j-m) = ((t(j+1) - x)/(t(j+1) - t(j-m+1))) * y(j-m+1);
-            for i = j-m+1:j-1
-                yn(i) = ((x - t(i))/(t(i+m) - t(i))) * y(i) + ...
-                    ((t(i+m+1) - x)/(t(i+m+1) - t(i+1))) * y(i+1);
+    y = zeros(length(x),n+k);
+    for a = 1:length(x)
+        j = chooseStartKnot(x(a),t,k);
+        yn = zeros(1,n+k);
+        y(a,j) = 1;
+        for b = 1:k
+            yn(j-b) = ((t(j+1) - x(a))/(t(j+1) - t(j-b+1))) * y(a,j-b+1);
+            for c = j-b+1:j-1
+                yn(c) = ((x(a) - t(c))/(t(c+b) - t(c))) * y(a,c) + ...
+                    ((t(c+b+1) - x(a))/(t(c+b+1) - t(c+1))) * y(a,c+1);
             end
-            yn(j) = ((x - t(j))/(t(j+m) - t(j))) * y(j);
-            y = yn;
+            yn(j) = ((x(a) - t(j))/(t(j+b) - t(j))) * y(a,j);
+            y(a,:) = yn;
         end
     end
 end
