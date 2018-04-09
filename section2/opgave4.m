c172db89b5454305c870cb2f5b2000aa1952f835
Elias Storme <elias.storme@gmail.com>
2018-04-09 14:27:27 +0200

    changed to include residue and rerendered

--- section2/opgave4.m
+++ section2/opgave4.m
@@ -3,10 +3,14 @@ x = linspace(0,2*pi);
 t = [-pi -2*pi/3 -1*pi/3 0 pi/4 2*pi/4 3*pi/4 pi 5*pi/4 6*pi/4 7*pi/4 2*pi 7*pi/3 8*pi/3 3*pi];
 f = sin(x).';
 y = kkb_spline(t,x,f,x,k);
+hold on
+yyaxis left
 plot(x,y,':','LineWidth',4);
 xlim([0 2*pi])
 ylim([-1 1])
-hold on
 plot(x,f);
+yyaxis right
+residu = f - y;
+plot(x,residu);
 print('sine_cubic','-depsc','-tiff');
 hold off
