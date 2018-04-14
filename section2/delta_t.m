8994a01ef7b8a4c75a0fe7434d8d5f28f7c0e965
Elias Storme <elias.storme@gmail.com>
2018-04-14 23:41:59 +0200

    corrected evaluation mistake

--- section2/delta_t.m
+++ section2/delta_t.m
@@ -2,7 +2,7 @@ function T = delta_t(t,j,k)
     T = zeros(length(t)-(k+1),k);
     for b = 1:k
         for a = j-b+1:j
-            T(a,b) = (t(a+b) - t(a));
+            T(a,b) = (t(a+b) - t(a))^-1;
         end
     end
 end
